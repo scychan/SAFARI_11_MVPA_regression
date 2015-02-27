c68b9928e31df59d4b6845dd53023f52d9e4f334
Stephanie Chan <scychan@princeton.edu>
2015-02-27 15:25:01 -0500

    searchlight - rungroups - set_params_and_run_mvpa

--- searchlight/run_mvpa.m
+++ searchlight/run_mvpa.m
@@ -2,7 +2,7 @@ function [] = run_mvpa(args)
 
 %% set paths
 
-data_dir = sprintf('../data/CLO%i',args.subjID);
+data_dir = sprintf('../../data/SFR%i',args.subjID);
 
 %% initialize the 'subj' structure
 
@@ -17,32 +17,46 @@ subj = set_mat(subj,'mask',maskname,args.mask);
 
 %% load selectors (run 1, run 2, etc)
 
-runsname = ['runs_' args.runs];
-load(fullfile(data_dir,runsname))
+if isempty(args.runs)
+    runsname = 'runs';
+else
+    runsname = ['runs_' args.runs];
+end 
+load(fullfile(data_dir,runsname)) % load 'runs'
 
 selsname = 'runs';
 subj = init_object(subj,'selector',selsname);
 subj = set_mat(subj,'selector',selsname,runs); 
-clear runs
 
 %% load conditions (regressors)
-% 3 conditions: C,L,O  
 
 load(fullfile(data_dir,args.regs))
-regsname = 'conds';
+regsname = 'posteriors';
 subj = init_object(subj,'regressors',regsname);
 subj = set_mat(subj,'regressors',regsname,regs);
 clear regs
-
-condnames = {'celebrities','locations','objects'};
-subj = set_objfield(subj,'regressors',regsname,'condnames',condnames);
 
 %% shift or convolve regressors
 % to account for hemodynamic lag
 % ( regsname => regsname_convolved OR regsname_shX )
 
-subj = shift_regressors(subj,regsname,selsname,args.shiftTRs);
-regsname = sprintf('%s_sh%i',regsname,args.shiftTRs);
+if strcmp(args.shiftTRs,'convolve')
+    HRF = spm_hrf(args.info.TR);
+    curr_regs = get_mat(subj,'regressors',regsname);
+    [ncond nTR] = size(curr_regs);
+    for isector = 1:ncond
+        convolved(isector,:) = conv(curr_regs(isector,:),HRF);
+    end
+    convolved = convolved(:,1:nTR);
+    
+    regsname = sprintf('%s_convolved',regsname);
+    subj = initset_object(subj,'regressors',regsname,convolved);
+    
+    clear curr_regs convolved
+else
+    subj = shift_regressors(subj,regsname,selsname,args.shiftTRs);
+    regsname = sprintf('%s_sh%i',regsname,args.shiftTRs);
+end
 
 %% create no_rest selector
 % => actives_selsname  ( a separate selector from 'runs' )
@@ -54,9 +68,15 @@ actives_selname = [regsname '_norest'];
 
 epiname = 'epi';
 subj = load_spm_pattern(subj,epiname,maskname,...
-    fullfile(data_dir,'filtered_func_data.nii'));
+    fullfile(data_dir,'big4D_mc_fmu_runs.nii'));
 
-%% no spatial smoothing
+%% spatial smoothing
+
+if args.fwhm~=0
+    subj = smooth_pattern(subj,epiname,args.fwhm,1);
+    subj = remove_mat(subj,'pattern',epiname); % remove the unsmoothed pattern from memory
+    epiname = sprintf('%s_sm%d',epiname,args.fwhm);
+end
 
 %% z-score the functional data
 % for each voxel: normalize timecourse to mean 0 and variance 1
@@ -65,7 +85,6 @@ subj = load_spm_pattern(subj,epiname,maskname,...
 if args.zscore == 1
     subj = zscore_runs(subj,epiname,selsname);
     subj = remove_mat(subj,'pattern',epiname); % remove the un-zscored pattern from memory
-    %subj = move_pattern_to_hd(subj, epiname);
     epiname = [epiname '_z'];
 end
 
@@ -83,17 +102,46 @@ featselname = maskname;
 
 %% classification
 
-class_args.train_funct_name = 'train_logreg';
-class_args.test_funct_name = 'test_logreg';
-class_args.penalty = args.penalty;
+% get class_args
+switch(args.classifier)
+    case 'gnb'
+        class_args.train_funct_name = 'train_gnb';
+        class_args.test_funct_name = 'test_gnb';
+        perfmet_func = 'perfmet_maxclass';
+    case 'bp'
+        class_args.train_funct_name = 'train_bp';
+        class_args.test_funct_name = 'test_bp';
+        class_args.nHidden = 0;
+        perfmet_func = 'perfmet_maxclass';
+    case 'L2logreg'
+        class_args.train_funct_name = 'train_L2_RLR';
+        class_args.test_funct_name = 'test_L2_RLR';
+        class_args.penalty = args.penalty;
+        class_args.lambda = 'crossvalidation'; % XX-- peeking??
+        perfmet_func = 'perfmet_maxclass';
+    case 'logreg'
+        class_args.train_funct_name = 'train_logreg';
+        class_args.test_funct_name = 'test_logreg';
+        class_args.penalty = args.penalty;
+        perfmet_func = 'perfmet_maxclass';
+    case 'ridge'
+        class_args.train_funct_name = 'train_ridge';
+        class_args.test_funct_name = 'test_ridge';
+        class_args.penalty = args.penalty;
+        perfmet_func = 'perfmet_xcorr';
+    otherwise
+        disp('(-) unknown classifier type!');
+        return
+end
 
 [subj results] = cross_validation(subj,epiname,regsname,...
-    xvalname,featselname,class_args);
+    xvalname,featselname,class_args,...
+    'perfmet_functs',perfmet_func);
 
 %% save results
 
 results_name = sprintf('vox%i_%s',args.ivox,datestr(now,'mmddyy_HHSS'));
-results_dir = sprintf('../results/radius%i/penalty%g/CLO%i/%s/searchlights/%s',...
+results_dir = sprintf('../../results/searchlights/radius%i/penalty%g/CLO%i/%s/searchlights/%s',...
     args.searchlight_radius,args.penalty,args.subjID,args.analysis,results_name);
 mkdir_ifnotexist(results_dir);
 
--- searchlight/submit2_rungroups.sh
+++ searchlight/submit2_rungroups.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
 # main options
-subjnums=$1 # "109 110 112 `seq 116 129`"
+subjnums=$1
 searchlight_radius=$2
 penalty=$3
  
 # run_mvpa for all the voxels (array job)
 for subjnum in $subjnums; do
     echo "subjnum = $subjnum"
-    . ../results/radius$searchlight_radius/penalty$penalty/CLO$subjnum/precomputations/groupstats.sh
+    . ../../results/searchlights/radius$searchlight_radius/penalty$penalty/SFR$subjnum/precomputations/groupstats.sh
     echo "ngroups = $ngroups"
-    for analysis in CLO_current_category CLO_preceding_category; do
- 	echo $analysis
-	if ismachine rondo; then
-	    submit $ngroups set_params_and_run_mvpa.m $subjnum $analysis searchlight_radius $searchlight_radius # array job
-	elif ismachine della; then
-	    for igroup in `seq 1 $ngroups`; do
-		echo $igroup
-		submit_short set_params_and_run_mvpa.m $subjnum $analysis searchlight_radius $searchlight_radius groupnum $igroup
-	    done
-	fi
-    done
+    if ismachine rondo; then
+	submit $ngroups set_params_and_run_mvpa.m $subjnum searchlight_radius $searchlight_radius # array job
+    elif ismachine della; then
+	for igroup in `seq 1 $ngroups`; do
+	    echo $igroup
+	    submit_short set_params_and_run_mvpa.m $subjnum searchlight_radius $searchlight_radius groupnum $igroup
+	done
+    fi
 done
