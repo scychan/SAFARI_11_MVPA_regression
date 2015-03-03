db7ba1e5b2600f3943a91625c431cb694b4256dc
Stephanie Chan <scychan@princeton.edu>
2015-03-02 23:57:19 -0500

    accept subjnums as input - submit1_precomputations.sh

--- searchlight/submit1_precomputations.sh
+++ searchlight/submit1_precomputations.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-searchlight_radius=$1
-penalty=$2
+subjnums=$1
+searchlight_radius=$2
+penalty=$3
 
 # main options
-subjnums="101" # 102 103 104 105 106 107 108 109 110 112 113 114 115 116 117 118 119 120 121 122 123 124 125 126 127 129 130 131 132 133 134"
 if ismachine rondo; then
     ngroups_max=5000
 elif ismachine della; then
@@ -14,6 +14,6 @@ fi
 
 # submit precomputations
 for subjnum in $subjnums; do
-    submit precomputations.m $subjnum $ngroups_max searchlight_radius $searchlight_radius penalty $penalty
+    submit_short precomputations.m $subjnum $ngroups_max searchlight_radius $searchlight_radius penalty $penalty
     sleep 1
 done
