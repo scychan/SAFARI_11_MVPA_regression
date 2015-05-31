a4722aedbcbbbf84a94c029865197c6af70215cf
Stephanie Chan <scychan@princeton.edu>
2015-05-31 16:52:34 -0400

    submit2_rungroups for Della as array jobs

--- searchlight/submit2_rungroups.sh
+++ searchlight/submit2_rungroups.sh
@@ -15,24 +15,10 @@ for subjnum in $subjnums; do
     . ../../results/searchlights/$analysis/radius$searchlight_radius/penalty$penalty/zscore$zscore/mask$mask/SFR$subjnum/precomputations/groupstats.sh
     echo "ngroups = $ngroups"
     if [ $niters = 0 ]; then
-	if ismachine rondo; then
-	    submit $ngroups set_params_and_run_mvpa.m $subjnum $analysis searchlight_radius $searchlight_radius penalty $penalty dozscore $zscore mask $mask # array job
-	elif ismachine della; then
-	    for igroup in `seq 1 $ngroups`; do
-		echo $igroup
-		submit_short set_params_and_run_mvpa.m $subjnum $analysis searchlight_radius $searchlight_radius penalty $penalty dozscore $zscore mask $mask groupnum $igroup
-	    done
-	fi
+	submit $ngroups set_params_and_run_mvpa.m $subjnum $analysis searchlight_radius $searchlight_radius penalty $penalty dozscore $zscore mask $mask # array job
     else
 	for iteration in `seq 1 $niters`; do
-	    if ismachine rondo; then
-		submit $ngroups set_params_and_run_mvpa.m $subjnum $analysis searchlight_radius $searchlight_radius penalty $penalty dozscore $zscore mask $mask iteration $iteration # array job
-	    elif ismachine della; then
-		for igroup in `seq 1 $ngroups`; do
-		    echo $igroup
-		    submit_short set_params_and_run_mvpa.m $subjnum $analysis searchlight_radius $searchlight_radius penalty $penalty dozscore $zscore mask $mask groupnum $igroup iteration $iteration
-		done
-	    fi
+	    submit_short $ngroups set_params_and_run_mvpa.m $subjnum $analysis searchlight_radius $searchlight_radius penalty $penalty dozscore $zscore mask $mask iteration $iteration # array job
 	done
     fi
 done
